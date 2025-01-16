c7ec5acb4ced5b1dd265b045ca4e8f94216bf841
jmanuel3 <josem199806@gmail.com>
2025-01-16 11:13:55 +1100

    Maqueto navbar a falta de retoques

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,12 +1,14 @@
 import { Container, Nav, Navbar, NavbarBrand } from "react-bootstrap";
 import { Link, NavLink } from "react-router";
-import logo from "../../assets/logoBors.png.png";
-
+import logo from "../../assets/borsLogo.png";
 const Menu = () => {
   return (
-    <Navbar expand="lg" className="bg-body-success">
+    <Navbar expand="lg" className="bg-body-tertiary py-2">
       <Container>
-        <Link to={"/"} className="navbar-brand">
+        <Link
+          to={"/"}
+          className="navbar-brand d-flex align-items-center justify-content-between"
+        >
           <img
             src={logo}
             alt="logo BorsGaming"
@@ -14,12 +16,15 @@ const Menu = () => {
             width={150}
           />
         </Link>
-        <Nav className="me-5">
-          <Nav.Link href="#home">Inicio</Nav.Link>
-          <Nav.Link href="#features">Administrador</Nav.Link>
-          <Nav.Link href="#pricing">Acerca de nosotros</Nav.Link>
-          <Nav.Link href="#pricing">Login</Nav.Link>
-        </Nav>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto gap-3">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#features">Administrador</Nav.Link>
+            <Nav.Link href="#pricing">Acerca de nosotros</Nav.Link>
+            <Nav.Link href="#login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
