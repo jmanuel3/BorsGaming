9bc21dfdcc8bd8640b0123b58222aa1663d2cdec
jmanuel3 <josem199806@gmail.com>
2025-03-04 11:37:01 +0800

    Guardo a falta de cambios en componente login

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,9 +1,32 @@
-import CardJuego from './Juego/CardJuego'
+import { useEffect, useState } from "react";
+import { listarJuegoAPI } from "../helpers/queries";
+import { Container, Row, Col } from "react-bootstrap";
+import CardJuego from "../pages/Juego/CardJuego";
+
 const Inicio = () => {
+  const [juegos, setJuegos] = useState([]);
+
+  useEffect(() => {
+    obtenerJuegos();
+  }, []);
+
+  const obtenerJuegos = async () => {
+    const respuesta = await listarJuegoAPI();
+    if (respuesta.status === 200) {
+      const datos = await respuesta.json();
+      setJuegos(datos);
+    }
+  };
+
   return (
-    <>
-    <CardJuego></CardJuego>
-    </>
+    <Container className="my-4">
+      <h2 className="text-light">Catálogo de Juegos</h2>
+      <Row md={3} lg={4} className="g-4">
+        {juegos.map((juego) => (
+          <CardJuego key={juego.id} juego={juego} />
+        ))}
+      </Row>
+    </Container>
   );
 };
 
--- src/components/pages/Juego/CardJuego.jsx
+++ src/components/pages/Juego/CardJuego.jsx
@@ -1,124 +1,36 @@
-import { Button, Card, Col, Row, Container } from "react-bootstrap";
-import undisputed from "../../../assets/undisputed.webp";
-import tekken from "../../../assets/tekken.webp";
-import hellLetLoose from "../../../assets/hellLetLoose.webp";
-import {
-  Cart3,
-  PlusCircle,
-  SuitHeart,
-  SuitHeartFill,
-} from "react-bootstrap-icons";
+import { Button, Card, Col } from "react-bootstrap";
+import { Cart3, PlusCircle, SuitHeart } from "react-bootstrap-icons";
+import { Link } from "react-router-dom";
 
-const CardJuego = () => {
+const CardJuego = ({ juego }) => {
   return (
-    <Container className="my-4">
-      <Row md={4} lg={3} className="g-4">
-        {/* Card 1 */}
-        <Col>
-          <Card className="h-100 bg-secondary">
-            <Card.Img
-              variant="top"
-              src={undisputed}
-              className="img-fluid rounded"
-              alt="Imagen de videojuego Undisputed©"
-            />
-            <Card.Body className="text-light d-flex flex-column">
-              <Card.Title>
-                <strong>Undisputed©</strong>
-              </Card.Title>
-              <Card.Text>
-                Undisputed es un videojuego de boxeo realista que destaca por
-                sus gráficos impresionantes, mecánicas estratégicas y físicas
-                avanzadas, ofreciendo una experiencia auténtica del deporte con
-                boxeadores y arenas oficiales. 🥊
-              </Card.Text>
-              <section className="d-flex gap-2 mt-auto">
-                <Button variant="dark" size="md" className="w-100">
-                  Añadir al carrito <Cart3 />
-                </Button>
-                <Button variant="dark" size="md" className="w-100">
-                  Añadir a favoritos <SuitHeart />
-                </Button>
-                <Button variant="dark" size="md" className="w-100">
-                  Detalles del Juego <PlusCircle />
-                </Button>
-              </section>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        {/* Card 2 */}
-        <Col>
-          <Card className="h-100 bg-secondary">
-            <Card.Img
-              variant="top"
-              src={tekken}
-              className="img-fluid rounded"
-              alt="Imagen de videojuego Tekken"
-            />
-            <Card.Body className="text-light d-flex flex-column">
-              <Card.Title>
-                <strong>Tekken</strong>
-              </Card.Title>
-              <Card.Text>
-                Tekken es un icónico videojuego de lucha desarrollado por Bandai
-                Namco. Destaca por su elenco de personajes únicos, combates
-                dinámicos en 3D y un enfoque en la estrategia y los combos. Es
-                uno de los pilares del género de juegos de pelea desde su debut
-                en 1994. 🥋🎮
-              </Card.Text>
-              <section className="d-flex gap-2 mt-auto">
-                <Button variant="dark" size="md" className="w-100">
-                  Añadir al carrito <Cart3 />
-                </Button>
-                <Button variant="dark" size="md" className="w-100">
-                  Añadir a favoritos <SuitHeart />
-                </Button>
-                <Button variant="dark" size="md" className="w-100">
-                  Detalles del Juego <PlusCircle />
-                </Button>
-              </section>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        {/* Card 3 */}
-        <Col>
-          <Card className="h-100 bg-secondary">
-            <Card.Img
-              variant="top"
-              src={hellLetLoose}
-              className="img-fluid rounded"
-              alt="Imagen de videojuego Hell Let Loose"
-            />
-            <Card.Body className="text-light d-flex flex-column">
-              <Card.Title>
-                <strong>Hell Let Loose</strong>
-              </Card.Title>
-              <Card.Text>
-                Hell Let Loose es un videojuego de disparos táctico ambientado
-                en la Segunda Guerra Mundial. Ofrece batallas masivas con hasta
-                100 jugadores por partida, combinando estrategia en tiempo real
-                y combates intensos en mapas realistas basados en campos de
-                batalla históricos. Su enfoque en el trabajo en equipo y la
-                autenticidad lo distingue en el género. 🎖️
-              </Card.Text>
-              <section className="d-flex gap-2 mt-auto">
-                <Button variant="dark" size="md" className="w-100">
-                  Añadir al carrito <Cart3 />
-                </Button>
-                <Button variant="dark" size="md" className="w-100">
-                  Añadir a favoritos <SuitHeart />
-                </Button>
-                <Button variant="dark" size="md" className="w-100">
-                  Detalles del Juego <PlusCircle />
-                </Button>
-              </section>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <Col>
+      <Card className="h-100 bg-secondary">
+        <Card.Img
+          variant="top"
+          src={juego.imagen}
+          className="img-fluid rounded"
+          alt={`Imagen de ${juego.nombreProducto}`}
+        />
+        <Card.Body className="text-light d-flex flex-column">
+          <Card.Title>
+            <strong>{juego.nombreProducto}</strong>
+          </Card.Title>
+          <Card.Text>{juego.descripcion}</Card.Text>
+          <section className="d-flex gap-2 mt-auto">
+            <Button variant="dark" size="md" className="w-100">
+              Añadir al carrito <Cart3 />
+            </Button>
+            <Button variant="dark" size="md" className="w-100">
+              Añadir a favoritos <SuitHeart />
+            </Button>
+            <Link to={`/detalle/${juego.id}`} className="btn btn-dark w-100">
+              Detalles del Juego <PlusCircle />
+            </Link>
+          </section>
+        </Card.Body>
+      </Card>
+    </Col>
   );
 };
 
